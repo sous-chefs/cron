e77c2ddfd76b82fb4c537417756d22f0af210c0c
jtimberman <joshua@opscode.com>
2012-08-31 18:55:17 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs cron"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.4"
+version          "1.0.5"
 
 %w{redhat centos scientific fedora amazon debian ubuntu}.each do |os|
   supports os
