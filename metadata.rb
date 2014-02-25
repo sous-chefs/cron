e62749ac79392d22610e4d54ed5538347f441ba2
Sean OMeara <someara@opscode.com>
2014-02-25 13:50:47 -0500

    Version bump to 1.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs cron"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.9"
+version          "1.3.0"
 
 %w{redhat centos scientific fedora amazon debian ubuntu raspbian solaris2}.each do |os|
   supports os
