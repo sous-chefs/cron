4232a94e9390735323268e879b523127a58acb1b
Matthias Arnason <matthias@arnason.org>
2014-03-20 16:17:39 -0700

    Clean up validation code for COOK-4337
    Specifically, split validate_nameable into two methods since the two use cases didn't share any code. Additionally, remove duplicates of validate_numeric() code present in validate_nameable.

--- resources/d.rb
+++ resources/d.rb
@@ -25,8 +25,8 @@ attribute :cookbook, :kind_of => String, :default => "cron"
 attribute :minute, :kind_of => [Integer, String], :default => "*", :callbacks => {"should be a valid minute spec" => lambda { |spec| validate_numeric(spec,0,59) } }
 attribute :hour, :kind_of => [Integer, String], :default => "*", :callbacks => {"should be a valid hour spec" => lambda { |spec| validate_numeric(spec,0,23) } }
 attribute :day, :kind_of => [Integer, String], :default => "*", :callbacks => {"should be a valid day spec" => lambda { |spec| validate_numeric(spec,1,31) } }
-attribute :month, :kind_of => [Integer, String], :default => "*", :callbacks => {"should be a valid month spec" => lambda { |spec| validate_nameable(spec,'mon') } }
-attribute :weekday, :kind_of => [Integer, String], :default => "*", :callbacks => {"should be a valid weekday spec" => lambda { |spec| validate_nameable(spec,'dow') } }
+attribute :month, :kind_of => [Integer, String], :default => "*", :callbacks => {"should be a valid month spec" => lambda { |spec| validate_month(spec,'mon') } }
+attribute :weekday, :kind_of => [Integer, String], :default => "*", :callbacks => {"should be a valid weekday spec" => lambda { |spec| validate_dow(spec,'dow') } }
 
 attribute :command, :kind_of => String, :required => true
 
@@ -56,42 +56,20 @@ def self.validate_numeric(spec,min,max)
   return true
 end
 
-def self.validate_nameable(spec,type)
-  if type == "mon" then
-    # Named abbreviations are permitted but not as part of a range or with stepping
-    if ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"].include? spec.downcase then
-      return true
-    end
-    min = 1
-    max = 12
-    spec.split(/\/|-|,/).each do |x|
-      next if x == '*'
-      if x.to_i.to_s == x
-        x = x.to_i
-        return false unless x >= min and x <= max
-      else
-        return false
-      end
-    end
+def self.validate_month(spec)
+  # Named abbreviations are permitted but not as part of a range or with stepping
+  if ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"].include? spec.downcase then
     return true
   end
+  # 1-12 are legal for months
+  return validate_numeric(spec,1,12)
+end
 
-  if type == "dow" then
-    # Named abbreviations are permitted but not as part of a range or with stepping
-    if ["sun", "mon", "tue", "wed", "thu", "fri", "sat"].include? spec.downcase then
-      return true
-    end
-    min = 0
-    max = 6
-    spec.split(/\/|-|,/).each do |x|
-      next if x == '*'
-      if x.to_i.to_s == x
-        x = x.to_i
-        return false unless x >= min and x <= max
-      else
-        return false
-      end
-    end
+def self.validate_dow(spec)
+  # Named abbreviations are permitted but not as part of a range or with stepping
+  if ["sun", "mon", "tue", "wed", "thu", "fri", "sat"].include? spec.downcase then
     return true
   end
+  # 0-6 are legal for days of week
+  return validate_numeric(spec,0,6)
 end
