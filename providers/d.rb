6509d05328a1e67ff717995fece5934e53f4ab55
Wilson Felipe <wfelipe@gmail.com>
2014-04-10 19:28:22 -0400

    [COOK-4544] fixing NameError exception on :delete action
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- providers/d.rb
+++ providers/d.rb
@@ -18,10 +18,10 @@
 #
 
 action :delete do
-  file "/etc/cron.d/#{new_resource.name}" do
+  f = file "/etc/cron.d/#{new_resource.name}" do
     action :delete
   end
-  new_resource.updated_by_last_action(t.updated_by_last_action?)
+  new_resource.updated_by_last_action(f.updated_by_last_action?)
 end
 
 action :create do
