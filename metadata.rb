0def4819745e06981dff0e98fa63fdfe354ae698
Sean OMeara <someara@opscode.com>
2014-04-23 18:21:14 -0700

    Version bump to 1.3.13 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscode.com'
 license 'Apache 2.0'
 description 'Installs cron'
 
-version '1.3.12'
+version '1.3.13'
 
 recipe 'cron', 'Installs the cron package and starts the crond service.'
 
