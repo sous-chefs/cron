d428c98ac4385ca0fa49f0d76cea36e5b61cda80
Christopher Webber <cwebberops@gmail.com>
2014-09-15 08:19:45 -0700

    v1.5.0 Release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscode.com'
 license 'Apache 2.0'
 description 'Installs cron'
 
-version '1.4.3'
+version '1.5.0'
 
 recipe 'cron', 'Installs the cron package and starts the crond service.'
 
