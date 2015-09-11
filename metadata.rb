1e87df90f0e59ae3b2377a1063da65fee4d7599d
Tim Smith <tsmith84@gmail.com>
2015-09-11 10:48:59 -0700

    Resolve rubocop warning

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs cron'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.6.1'
 
 recipe 'cron', 'Installs the cron package and starts the crond service.'
