f238187fd635597de5b18586d33836c5b12d95b7
Laurenza, Vito <vlaurenza@webmd.net>
2015-10-16 15:47:40 -0700

    add additional Serverspec examples

--- test/integration/default/serverspec/localhost/cron_spec.rb
+++ test/integration/default/serverspec/localhost/cron_spec.rb
@@ -16,26 +16,65 @@ if %w(debian ubuntu fedora redhat).include?(os[:family])
   end
 
   describe service(service_name) do
-    it { should be_enabled }
-    it { should be_running }
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
   end
 
   describe package(package_name) do
-    it { should be_installed }
+    it { is_expected.to be_installed }
   end
 end
 
 if os[:family] == 'freebsd'
   describe file('/etc/crontab') do
-    its(:content) { should match /\* \* \* \* \* appuser \/bin\/true/ }
+    its(:content) { is_expected.to match /\* \* \* \* \* appuser \/bin\/true/ }
+    its(:content) { is_expected.to match /0 1 2 \* \* appuser \/bin\/true/ }
+    its(:content) { is_expected.to match /\@midnight appuser \/bin\/true/ }
+
+    its(:content) do
+      is_expected.to match /\*\/5 1,23 2-5 \* \* appuser \/srv\/app\/scripts\/generate_report/
+    end
+
+    its(:content) do
+      is_expected.to match /0 6 \* 1 \* appuser \/srv\/app\/scripts\/generate_report/
+    end
+
+    its(:content) do
+      is_expected.to match /45 13 \* \* 7 appuser \/srv\/app\/scripts\/generate_report/
+    end
   end
 else
-  # make sure the :create_if_missing didn't overwrite the :create
-  describe file('/etc/cron.d/test-weekday-usage-report') do
-    its(:content) { should match /\/srv\/app\/scripts\/generate_report/ }
+  describe file('/etc/cron.d/bizarrely-scheduled-usage-report') do
+    its(:content) do
+      is_expected.to match /\*\/5 1,23 2-5 \* \* appuser \/srv\/app\/scripts\/generate_report/
+    end
   end
-  describe file('/etc/cron.d/nil_value_check') do
-    its(:content) { should match /\* \* \* \* \* appuser \/bin\/true/ }
+
+  describe file('/etc/cron.d/test-month-usage-report') do
+    its(:content) do
+      is_expected.to match /0 6 \* 1 \* appuser \/srv\/app\/scripts\/generate_report/
+    end
+  end
+
+  describe file('/etc/cron.d/test-weekday-usage-report') do
+    # make sure the :create_if_missing didn't overwrite the :create
+    its(:content) do
+      is_expected.to match /\/srv\/app\/scripts\/generate_report/
+    end
+  end
+
+  describe file('/etc/cron.d/fixnum-job') do
+    its(:content) { is_expected.to match /0 1 2 \* \* appuser \/bin\/true/ }
+  end
+
+  describe file('/etc/cron.d/predefined_value_check') do
+    its(:content) { is_expected.to match /\@midnight appuser \/bin\/true/ }
+  end
+
+  %w(nil_value_check no_value_check).each do |n|
+    describe file("/etc/cron.d/#{n}") do
+      its(:content) { is_expected.to match /\* \* \* \* \* appuser \/bin\/true/ }
+    end
   end
   describe file('/etc/cron.d/delete_cron') do
     it { should_not exist }
