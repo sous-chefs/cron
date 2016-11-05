11b68e5c977e00fc559bf3faace9fddb49bc9662
Tim Smith <tsmith@chef.io>
2016-11-05 16:54:56 -0700

    Make sure we install the package on suse
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,7 +16,7 @@
 default['cron']['package_name'] = case node['platform_family']
                                   when 'debian'
                                     ['cron']
-                                  when 'rhel', 'fedora'
+                                  when 'rhel', 'fedora', 'suse'
                                     node['platform_version'].to_f >= 6.0 ? ['cronie'] : ['vixie-cron']
                                   when 'solaris2'
                                     'core-os'
