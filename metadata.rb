c4f46fa13c32636418ecd4f1f5a84d8def3777c8
Tim Smith <tsmith84@gmail.com>
2019-09-30 16:54:58 -0700

    Resolve cookstyle warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs cron'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '6.2.1'
-
-recipe 'cron', 'Installs the cron package and starts the crond service.'
 
 %w(ubuntu debian fedora redhat centos scientific oracle amazon smartos omnios solaris2 freebsd zlinux opensuse suse opensuseleap).each do |os|
   supports os
@@ -14,4 +11,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/cron'
 issues_url 'https://github.com/chef-cookbooks/cron/issues'
-chef_version '>= 12.7' if respond_to?(:chef_version)
+chef_version '>= 12.7'
